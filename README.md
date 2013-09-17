726be5f6d317492148fd8a653aab989ff8554361
ideawu <wuzuyang@wuzuyang.local>
2013-09-17 11:57:23 +0800

    update

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ A C/C++ comet server built with libevent
 make
 ./icomet
 
-curl -v "http://127.0.0.1:8000/sub?id=12"
+curl -v "http://127.0.0.1:8100/sub?id=12"
 # open another terminal
 curl -v "http://127.0.0.1:8000/pub?id=12&content=hi"
 ```
--- src/icomet.cpp
+++ src/icomet.cpp
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <errno.h>
 #include <evhttp.h>
 #include <event2/event.h>
 #include <event2/http.h>
 #include "util/log.h"
 #include "channel.h"
 #include "server.h"
+
+#define MAX_BIND_PORTS 10
 
 Server *serv;
 
@@ -32,18 +35,22 @@ int main(int argc, char **argv){
 	signal(SIGPIPE, SIG_IGN);
 
 	struct event_base *base;
-	struct evhttp *http;
+	struct evhttp *admin_http;
+	struct evhttp *front_http;
 	struct event *sigint_event;
 	struct event *sigterm_event;
 	struct event *timer_event;
-	unsigned short port = 8000;
+	
+	unsigned short admin_port = 8000;
+	unsigned short front_port = 8100;
+
+	log_info("starting icomet...");
 
 	base = event_base_new();
 	if(!base){
 		fprintf(stderr, "event_base_new() error!\n");
 		exit(0);
 	}
-	http = evhttp_new(base);
 	
 	sigint_event = evsignal_new(base, SIGINT, signal_cb, (void *)base);
 	if(!sigint_event || event_add(sigint_event, NULL)<0){
@@ -66,43 +73,67 @@ int main(int argc, char **argv){
 		}
 	}
 
-	// /sub?cb=js_callback&id=123&token=&[last_seq=123]
-	evhttp_set_cb(http, "/sub", sub_handler, NULL);
-	// /pub?id=123&content=hi or /pub?uid=xxx&content=hi
-	// content must be json encoded string without leading and trailing quotes
-	evhttp_set_cb(http, "/pub", pub_handler, NULL);
-	// 分配通道, 返回通道的id和token
-	// /sign?uid=xxx
-	// 销毁通道
-	// /close?id=123 or /close?uid=xxx
-	// 获取通道的信息
-	// /stat?id=123 or /stat?uid=xxx
-	// 判断通道是否处于被订阅状态(所有订阅者断开连接一定时间后, 通道才转为空闲状态)
-	// /check?id=123,234
-	
-	//evhttp_set_gencb(http, request_handler, NULL);
-	
-	// TODO: listen at two ports, accept sub from public port, others from admin port
-	
-	struct evhttp_bound_socket *handle;
-	handle = evhttp_bind_socket_with_handle(http, "0.0.0.0", port);
-	printf("server listen at 127.0.0.1:%d\n", port);
+	{
+		admin_http = evhttp_new(base);
+		// /pub?id=123&content=hi or /pub?uid=xxx&content=hi
+		// content must be json encoded string without leading and trailing quotes
+		evhttp_set_cb(admin_http, "/pub", pub_handler, NULL);
+		// 分配通道, 返回通道的id和token
+		// /sign?uid=xxx
+		// 销毁通道
+		// /close?id=123 or /close?uid=xxx
+		// 获取通道的信息
+		// /stat?id=123 or /stat?uid=xxx
+		// 判断通道是否处于被订阅状态(所有订阅者断开连接一定时间后, 通道才转为空闲状态)
+		// /check?id=123,234
+		/*
+		struct evhttp_bound_socket *handle = evhttp_bind_socket_with_handle(http, "0.0.0.0", port);
+		if(!handle){
+			log_fatal("bind admin_port %d error! %s", admin_port, strerror(errno));
+			exit(0);
+		}
+		*/
+		int ret = evhttp_bind_socket(admin_http, "0.0.0.0", admin_port);
+		if(ret == -1){
+			log_fatal("bind admin_port %d error! %s", admin_port, strerror(errno));
+			exit(0);
+		}
+		log_info("admin server listen on 127.0.0.1:%d", admin_port);
+	}
+
+
+	{
+		front_http = evhttp_new(base);
+		// /sub?cb=js_callback&id=123&token=&[last_seq=123]
+		evhttp_set_cb(front_http, "/sub", sub_handler, NULL);	
+		for(int i=0; i<MAX_BIND_PORTS; i++){
+			int port = front_port + i;
+			int ret = evhttp_bind_socket(front_http, "0.0.0.0", port);
+			if(ret == -1){
+				log_fatal("bind front_port %d error! %s", port, strerror(errno));
+				exit(0);
+			}
+			log_info("front server listen on 127.0.0.1:%d", port);
+		}
+	}
+
 
 	serv = new Server();
 	
+	log_info("icomet started");
 	event_base_dispatch(base);
 	log_debug("event dispatch stopped");
 	
 	event_free(timer_event);
 	event_free(sigint_event);
 	event_free(sigterm_event);
-	evhttp_free(http);
+	evhttp_free(admin_http);
+	evhttp_free(front_http);
 	event_base_free(base);
 
 	delete serv;
 
-	log_debug("quit");
-
+	log_info("icomet exit");
 	return 0;
 }
 
