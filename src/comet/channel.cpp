733f4348666f2d7fd2947eb76b88eec0b5c0a6af
ideawu <ideawu@local>
2014-11-10 10:37:11 +0800

    add copyright msg

--- src/comet/channel.cpp
+++ src/comet/channel.cpp
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 2012-2014 The SSDB Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+*/
 #include "channel.h"
 #include <event2/buffer.h>
 #include <event2/keyvalq_struct.h>
