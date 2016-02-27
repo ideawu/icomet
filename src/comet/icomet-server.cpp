c778c537832bb1b943fda7959979d4a6bf8ddc10
ideawu <ideawu@me>
2016-02-27 12:25:18 +0800

    update

--- src/comet/icomet-server.cpp
+++ src/comet/icomet-server.cpp
@@ -121,7 +121,7 @@ void ping_handler(struct evhttp_request *req, void *arg){
 
 void pub_handler(struct evhttp_request *req, void *arg){
 	CHECK_AUTH();
-	serv->pub(req, true);
+	serv->pub(req, false);
 }
 
 void push_handler(struct evhttp_request *req, void *arg){
